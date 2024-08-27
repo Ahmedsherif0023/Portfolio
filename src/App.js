83727e4daeaed227621455b62e5124da83978825
Ahmed Sherif <ahmedsherifoo23@gmail.com>
2024-08-28 01:40:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <>
+    <h1 className="x">SHEFO</h1>
+    <h1 className="x">SHEFO</h1>
+    <h1 className="x">SHEFO</h1>
+    </>
+  );
+}
+
+export default App;
