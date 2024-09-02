25b53022dda7edf111e13e78f4ac659f4265cc63
Ahmed Sherif <ahmedsherifoo23@gmail.com>
2024-09-02 04:37:01 +0300

    edited the about page

--- src/pages/About/aboutShefo.js
+++ src/pages/About/aboutShefo.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Header from '../../components/Header/header';
-import Footer from '../../components/Footer/footer';
+import React from "react";
+import Header from "../../components/Header/header";
+import Footer from "../../components/Footer/footer";
+import Photo from "../../assets/MyPhoto.png";
+import PDF from "../../CV.pdf";
+
 const AboutShefo = () => {
   return (
     <>
-      
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Document</title>
@@ -18,13 +20,51 @@ const AboutShefo = () => {
         href="mobile-screen.css"
         media="(max-width: 800px)"
       />
-      <Header/>
-      <main>
-        <h1>About Page</h1>
+      <Header />
+      <main className="abtme">
+        <h1 className="aboutme">About Me</h1>
+
+        <p className="info">A little bit info about me</p>
+        <br />
+  <ul className="infoUl">
+          <li>
+            <label className="a">Name:<label className="xzxc aa"> Ahmed Sherif</label></label>
+            
+          </li>
+          <li>
+            <label className="b">Date Of Birth:<label className="xzxc bb">December 08,2002</label></label>
+            
+          </li>
+          <li>
+            <label className="c">Address:<label className="xzxc cc">Tanta | El-gharbia | Egypt</label></label>
+            
+          </li>
+          <li>
+            <label className="d">Zip Code: <label className="xzxc dd">31511</label></label>
+            
+          </li>
+          <li>
+            <label className="e">E-mail:<label className="xzxc ee">ahmedsherifoo23@gmail.com</label></label>
+            
+          </li>
+          <li>
+            <label className="f">Phone:<label className="xzxc ff"> +201274375560</label></label>
+          </li>
+          <li><label className="f z"><label className="pj"> 10</label> Projects Completed</label></li>
+          <li> <a href="https://aqua-loleta-32.tiiny.site" target="_blank">
+            <button className="f z b">Download CV</button>
+          </a></li>
+  </ul>
+  <img className="img" src={Photo} alt="MyPhoto" />
+  
+  
+          {/* <a href={PDF} download="CV.pdf">
+            <button className="f z b">Download CV</button>
+          </a> */}
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
-}
+};
 
 export default AboutShefo;
